a11423c91eaa83b22dac06404a53f383e1d3a95c
User <User@172.17.248.91>
2017-06-12 09:28:19 +0800

    1.This class is to validate dates,gives number of working days removing saturday and sundays.

---
+++ src/main/java/edu/iss/t4laps/validator/LeaveDateValidator.java
@@ -0,0 +1,40 @@
+package edu.iss.t4laps.validator;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class LeaveDateValidator {
+	
+	
+	public static int getWorkingDaysBetweenTwoDates(Date startDate, Date endDate) {
+	    Calendar startCal = Calendar.getInstance();
+	    startCal.setTime(startDate);        
+
+	    Calendar endCal = Calendar.getInstance();
+	    endCal.setTime(endDate);
+
+	    int workDays = 0;
+
+	    //Return 0 if start and end are the same
+	    if (startCal.getTimeInMillis() == endCal.getTimeInMillis()) {
+	        return 0;
+	    }
+
+	    if (startCal.getTimeInMillis() > endCal.getTimeInMillis()) {
+	        startCal.setTime(endDate);
+	        endCal.setTime(startDate);
+	    }
+
+	    do {
+	       //excluding start date
+	    	//startCal.add(Calendar.DAY_OF_MONTH, 1);
+	        if (startCal.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY && startCal.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
+	            ++workDays;
+	        }
+	        startCal.add(Calendar.DAY_OF_MONTH, 1);
+	    } while (startCal.getTimeInMillis() <= endCal.getTimeInMillis()); //excluding end date
+
+	    return workDays;
+	}
+
+}
