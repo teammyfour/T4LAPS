9735e9818892282241d331a8e6d7603974577eac
ashpri <ashpri@172.17.249.219>
2017-06-12 13:33:52 +0800

    Created the user model class

---
+++ src/main/java/edu/iss/t4laps/model/User.java
@@ -0,0 +1,110 @@
+package edu.iss.t4laps.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+
+@Entity
+@Table(name = "user_details")
+public class User {
+	@Id
+	private String userId;
+	@Basic(optional = false)
+	@Column(name = "user_name")
+	private String name;
+	private String password;
+	private String employeeId;
+
+	@ManyToMany(targetEntity = UserRole.class, cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+	@JoinTable(name = "userrole", joinColumns = {
+			@JoinColumn(name = "userid", referencedColumnName = "userid") }, inverseJoinColumns = {
+					@JoinColumn(name = "roleid", referencedColumnName = "roleid") }
+
+	)
+	private List<UserRole> roleSet;
+	
+	@Transient
+	private ArrayList<String> roleIds = new ArrayList<String>();
+
+	public User() {
+	}
+
+	public User(String userId, String name, String password, String employeeId) {
+		this.userId = userId;
+		this.name = name;
+		this.password = password;
+		this.employeeId = employeeId;
+	}
+
+	public User(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public List<UserRole> getRoleSet() {
+		return roleSet;
+	}
+
+	public void setRoleSet(ArrayList<UserRole> roleSet) {
+		this.roleSet = roleSet;
+	}
+
+	public ArrayList<String> getRoleIds() {
+		ArrayList<UserRole> rList = (ArrayList<UserRole>) this.getRoleSet();
+		ArrayList<String> roleIds = new ArrayList<String>();
+		for (UserRole role : rList) {
+			roleIds.add(role.getRoleId());
+		}
+		return roleIds;
+	}
+
+	public void setRoleIds(ArrayList<String> roleIds) {
+
+		this.roleIds = roleIds;
+	}
+}
+
+	
