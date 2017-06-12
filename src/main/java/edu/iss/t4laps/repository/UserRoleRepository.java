49365d0a45881c6b8ad5e2c4d678a4eaa64dcbea
ashpri <ashpri@172.17.249.219>
2017-06-12 13:55:52 +0800

    Adding UserRepository and UserRoleRepository class to the repository package

---
+++ src/main/java/edu/iss/t4laps/repository/UserRoleRepository.java
@@ -0,0 +1,18 @@
+package edu.iss.t4laps.repository;
+
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import edu.iss.t4laps.model.UserRole;
+
+public interface UserRoleRepository extends  JpaRepository<UserRole, String> {
+	
+	@Query("SELECT r.name FROM Role r")
+	ArrayList<String> findAllRolesNames();
+	
+	@Query("SELECT r FROM Role r WHERE r.name = :name")
+	ArrayList<UserRole> findRoleByName(@Param("name") String name);
+}
