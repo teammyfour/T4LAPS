ab0870af8768cf05e9256a82da0341802597f3e8
ashpri <ashpri@172.17.249.219>
2017-06-12 16:28:49 +0800

    1.Adding UserRoleService, UserRoleServiceImpl, UserService,UserServiceImpl to the service package. 2.Adding UserRoleValidator and UserValidator to the validator package. 3.Adding UserSession to the controller package.

---
+++ src/main/java/edu/iss/t4laps/service/UserService.java
@@ -0,0 +1,21 @@
+package edu.iss.t4laps.service;
+
+import java.util.ArrayList;
+
+import edu.iss.t4laps.model.UserRole;
+import edu.iss.t4laps.model.User;
+
+public interface UserService {
+	
+	User findUser(String userId);
+	ArrayList<User> findAllUsers();
+	User createUser(User user);
+	User changeUser(User user);
+	void removeUser(User user);
+	ArrayList<UserRole> findRolesForUser(String userId);
+	ArrayList<String> findRoleNamesForUser(String userId);
+	ArrayList<String> findManagerNameByUID(String userId);	
+	User authenticate(String uname, String pwd);
+
+}
+
