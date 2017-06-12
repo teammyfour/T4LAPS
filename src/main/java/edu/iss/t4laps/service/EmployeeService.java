407da521247b19a09a0e79cb9f50456587d47a9b
mabejeok <mavecheok@gmail.com>
2017-06-12 14:45:31 +0800

    Committed EmployeeService, EmployeeServiceImpl and Employee Repository

---
+++ src/main/java/edu/iss/t4laps/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package edu.iss.t4laps.service;
+
+import java.util.ArrayList;
+
+import edu.iss.t4laps.model.EmployeeDetails;
+
+public interface EmployeeService {
+	EmployeeDetails createEmployee(EmployeeDetails employee);
+	
+	ArrayList<EmployeeDetails> findAllEmployees();
+	
+	EmployeeDetails findEmployee(int employeeId);
+	
+	void removeEmployee(EmployeeDetails employee);
+
+	EmployeeDetails changeEmployee(EmployeeDetails employee);
+}
