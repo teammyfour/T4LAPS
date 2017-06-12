6ecce147f2637646752719cd967b8fe584514dc8
ashpri <ashpri@172.17.249.219>
2017-06-12 14:34:14 +0800

    Adding some changes to UserRoleComparator

--- src/main/java/edu/iss/t4laps/model/comparator/UserRoleComparator.java
+++ src/main/java/edu/iss/t4laps/model/comparator/UserRoleComparator.java
@@ -4,11 +4,13 @@ import java.util.Comparator;
 
 import edu.iss.t4laps.model.UserRole;
 
-public abstract class UserRoleComparator  implements Comparator<UserRole>{
+public class UserRoleComparator  implements Comparator<UserRole>{
 
 	@Override
 	public int compare(UserRole o1, UserRole o2) {
+		// TODO Auto-generated method stub
 		return o1.getName().compareTo(o2.getName());
 	}
 
 }
+
